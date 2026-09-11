65a4d92d17a501f17929c8bbf0e33472e018dea5
User <parpievislam99@gmail.com>
2026-09-11 22:01:04 +0500

    Добавил файлы лабораторной 1

---
+++ labweb1/Controllers/CoursesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IPweblab1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] 
+    public class CoursesController : ControllerBase
+    {
+        private static readonly List<Course> _courses = new List<Course>
+        {
+            new Course { Id = 1, Name = "Web Services", Teacher = "Teacher 1", Credits = 5 },
+            new Course { Id = 2, Name = "Databases", Teacher = "Teacher 2", Credits = 4 }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Course>> GetAll()
+        {
+            return Ok(_courses);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Course> GetById(int id)
+        {
+            var course = _courses.FirstOrDefault(x => x.Id == id);
+
+            if (course == null)
+            {
+                return NotFound(); 
+            }
+
+            return Ok(course); 
+        }
+    }
+}
