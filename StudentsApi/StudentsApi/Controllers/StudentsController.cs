6e1ff24a53db5c4695bfdb264d07d12defd044d2
User <parpievislam99@gmail.com>
2026-09-17 21:01:01 +0500

    прак 1

---
+++ StudentsApi/StudentsApi/Controllers/StudentsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentsApi.Models;
+
+namespace StudentsApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        private static List<Student> students = new()
+        {
+            new Student
+            {
+                Id = 1,
+                Name = "Alex",
+                Group = "SE-301"
+            },
+
+            new Student
+            {
+                Id = 2,
+                Name = "Anna",
+                Group = "SE-302"
+            },
+
+            new Student
+            {
+                Id = 3,
+                Name = "Max",
+                Group = "SE-301"
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Student>> GetStudents()
+        {
+            return Ok(students);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Student> GetStudent(int id)
+        {
+            var student = students.FirstOrDefault(s => s.Id == id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public ActionResult<Student> CreateStudent(Student student)
+        {
+            students.Add(student);
+
+            return Ok(student);
+        }
+    }
+}
